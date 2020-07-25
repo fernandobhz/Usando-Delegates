40c2f25c55e5dddfca0980777af7c7aa0cc708c5
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 19:28:45 -0300

    old project migrated

---
+++ Usando Delegates/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Inherits FormularioCrud
+
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.Registrar(Me, AddressOf Ola)
+    End Sub
+
+    Sub Ola()
+        MsgBox("Olá")
+    End Sub
+
+
+End Class
+
+
+Public Class FormularioCrud
+    Inherits Form
+
+    Private Filho As Form
+
+    Delegate Sub S1()
+
+    Sub Registrar(filho As Form, s1 As S1)
+        Me.Filho = filho
+
+        s1.Invoke()
+    End Sub
+
+End Class
